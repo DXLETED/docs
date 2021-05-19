e7725d819904342e2bf46c01ccd9019cb2c59297
DXLETED <xdeleted.al@gmail.com>
2021-05-19 15:20:19 +0300

    Signers 2

--- client/src/components/document/DocumentSigners.tsx
+++ client/src/components/document/DocumentSigners.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import st from 'styles/components/document/DocumentSigners.module.sass'
 import { documentActions } from 'store/document'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,8 +6,39 @@ import { Modal } from 'components/Modal'
 import { useDispatchTyped } from 'hooks/dispatchTyped.hook'
 import { useSelectorTyped } from 'hooks/selectorTyped.hook'
 import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd'
-import { faCheck, faUserEdit } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faChevronDown, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { useAuth } from 'hooks/auth.hook'
+import clsx from 'clsx'
+import { useOnClickOutside } from 'hooks/onClickOutside.hook'
+
+const UsersSelect: React.FC = () => {
+  const ref = useRef<HTMLDivElement | any>(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const dispatch = useDispatchTyped()
+  const signers = useSelectorTyped(s => s.document.document.signers)
+  const { user } = useAuth()
+  const users = useSelectorTyped(s => s.users)
+  useOnClickOutside<HTMLDivElement>(ref, () => isOpen && setIsOpen(false))
+  const add = (userId: string) => {
+    dispatch(documentActions.addSigner({ userId }))
+    setIsOpen(false)
+  }
+  return (
+    <div className={clsx(st.usersSelect, { [st.open]: isOpen })} onClick={() => setIsOpen(true)} ref={ref}>
+      <div className={st.label}>Добавить пользователя</div>
+      <FontAwesomeIcon className={st.arrow} icon={faChevronDown} />
+      <div className={st.dropdown}>
+        {users
+          .filter(u => u.userId !== user?.userId && !signers.includes(u.userId))
+          .map((user, i) => (
+            <div className={st.el} onClick={() => add(user.userId)} key={user.userId}>
+              {user.username}
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
 
 interface DocumentSignersProps {
   users: { userId: string; username: string }[]
@@ -15,11 +46,10 @@ interface DocumentSignersProps {
 export const DocumentSigners: React.FC<DocumentSignersProps> = ({ users }) => {
   const dispatch = useDispatchTyped()
   const signers = useSelectorTyped(s => s.document.document.signers)
-  const { user } = useAuth()
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   const move = (result: DropResult): any =>
     result.destination &&
     dispatch(documentActions.moveSigner({ prev: result.source.index, new: result.destination.index }))
+  const remove = (userId: string) => dispatch(documentActions.delSigner({ userId }))
   return (
     <div className={st.signers}>
       <div className={st.label}>Подписанты</div>
@@ -31,13 +61,13 @@ export const DocumentSigners: React.FC<DocumentSignersProps> = ({ users }) => {
                 {signers.map((userId, i) => (
                   <Draggable draggableId={userId} index={i} key={userId}>
                     {provided => (
-                      <div
-                        className={st.signer}
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        key={userId}>
-                        {users.find(u => u.userId === userId)?.username}
+                      <div className={st.signer} ref={provided.innerRef} {...provided.draggableProps} key={userId}>
+                        <div className={st.username} {...provided.dragHandleProps}>
+                          {users.find(u => u.userId === userId)?.username}
+                        </div>
+                        <div className={st.remove} onClick={() => remove(userId)}>
+                          <FontAwesomeIcon icon={faTrash} />
+                        </div>
                       </div>
                     )}
                   </Draggable>
@@ -48,35 +78,7 @@ export const DocumentSigners: React.FC<DocumentSignersProps> = ({ users }) => {
           )}
         </Droppable>
       </DragDropContext>
-      <div className={st.add} onClick={() => setIsModalOpen(true)}>
-        <FontAwesomeIcon className={st.addIcon} icon={faUserEdit} size="sm" />
-        Выбрать
-      </div>
-      <Modal title="Пользователи" isOpen={isModalOpen} close={() => setIsModalOpen(false)}>
-        <div className={st.users}>
-          {users
-            .filter(u => u.userId !== user?.userId)
-            .map(u => (
-              <div
-                className={st.user}
-                onClick={() =>
-                  dispatch(
-                    signers.find(s => s === u.userId)
-                      ? documentActions.delSigner({ userId: u.userId })
-                      : documentActions.addSigner({ userId: u.userId })
-                  )
-                }
-                key={`${u.userId}`}>
-                <div className={st.check}>
-                  {signers.find(s => s === u.userId) && (
-                    <FontAwesomeIcon className={st.checkIcon} icon={faCheck} size="sm" />
-                  )}
-                </div>
-                {u.username}
-              </div>
-            ))}
-        </div>
-      </Modal>
+      <UsersSelect />
     </div>
   )
 }
